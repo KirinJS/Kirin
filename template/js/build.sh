952cd94a6623cb0a3b79c7454895adba1fa9bb46
Matt Gaunt <matt@gauntface.co.uk>
2011-10-26 13:01:09 +0100

    Adding a template directory for the javascript projects to use

---
+++ template/js/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+
+
+
+
+export PROJECT_NAME=<INSERT PROJECT NAME HERE>
+
+
+
+
+
+
+dirname=$(dirname $0)
+if [ "${KIRIN_HOME}" = "" ] ; then
+	# assume we're in the demos directory.
+	dir=$dirname/../../..
+	export KIRIN_HOME=$(cd "$dir"; echo `pwd`);
+fi
+export KIRINJS_HOME=${KIRIN_HOME}/js
+
+dist_path=$3
+if [ "$3" = "" ] ; then 
+	dist_path="-"
+fi
+
+export PROJECT_HOME=$(cd "${dirname}" ; echo `pwd`)
+
+// we can also add a fourth argument which will specify a build file to run just after the kirin files have run through JSLint.
+sh $KIRINJS_HOME/build/build.sh $1 $2 $dist_path 
