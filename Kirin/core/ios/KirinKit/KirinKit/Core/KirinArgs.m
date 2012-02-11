a8281acc9a27ad652b609ba1891341f3d29e1eac
jhugman <james@hugman.tv>
2012-02-11 19:24:03 +0000

    no message

---
+++ Kirin/core/ios/KirinKit/KirinKit/Core/KirinArgs.m
@@ -0,0 +1,56 @@
+//
+//  KirinArgs.m
+//  KirinKit
+//
+//  Created by James Hugman on 11/02/2012.
+//  Copyright (c) 2012 Future Platforms. All rights reserved.
+//
+
+#import "KirinArgs.h"
+
+#import "JSON.h"
+
+@implementation KirinArgs
+
++ (NSString*) string: (NSString*) string {
+    // TODO ensure quotes are correct.
+    return [NSString stringWithFormat:@"'%@'", string];
+}
+
++ (NSString*) safeString: (NSString*) string {
+    // TODO ensure quotes are correct.
+    return [NSString stringWithFormat:@"'%@'", string];
+}
+
++ (NSString*) array: (NSArray*) object {
+    return [object JSONRepresentation];
+}
+
++ (NSString*) object: (NSDictionary*) object {
+    return [object JSONRepresentation];
+}
+
++ (NSString*) integer: (int) number {
+    return [NSString stringWithFormat:@"%d", number];
+}
+
++ (NSString*) number: (NSNumber*) number {
+    return [NSString stringWithFormat:@"%@", number];
+}
+
++ (NSString*) boolean: (BOOL) boolean {
+    return boolean ? @"true" : @"false";
+}
+
++ (NSString*) args: (NSString*) argString, ... {
+    NSMutableArray* argStringList = [[[NSMutableArray alloc] init] autorelease];
+    va_list args;
+    va_start(args, argString);
+    for (NSString *arg = argString; arg != nil; arg = va_arg(args, NSString*)) {
+        [argStringList addObject:argString];        
+    }
+    va_end(args);
+    return [argStringList componentsJoinedByString:@", "];
+}
+
+@end
