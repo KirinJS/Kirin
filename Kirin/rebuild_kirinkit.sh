df84270f9fb93e5aefdbe36ea1255c96d2afe03b
jhugman <james@hugman.tv>
2012-03-08 17:16:37 +0000

    Runnable even if KIRIN_HOME is not set.

--- Kirin/rebuild_kirinkit.sh
+++ Kirin/rebuild_kirinkit.sh
@@ -1,2 +1,5 @@
+if [[ "$KIRIN_HOME" = "" ]] ; then 
+	export KIRIN_HOME=`dirname $0`
+fi
 rm -Rf $KIRIN_HOME/core/ios/KirinKit/build 2>/dev/null
 node $KIRIN_HOME/build.js --ios --ios-configuration Debug
