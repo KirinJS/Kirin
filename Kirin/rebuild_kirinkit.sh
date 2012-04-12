6b30b69cd69988480f361b57d8b68e319e777e99
jhugman <james@hugman.tv>
2012-04-12 11:15:32 +0100

    Make it not run the tests, not build the js, and allow different configurations to be built.

--- Kirin/rebuild_kirinkit.sh
+++ Kirin/rebuild_kirinkit.sh
@@ -1,5 +1,9 @@
 if [[ "$KIRIN_HOME" = "" ]] ; then 
 	export KIRIN_HOME=`dirname $0`
 fi
+configuration=$1
+if [[ "$configuration" = "" ]] ; then 
+	configuration=Debug
+fi
 rm -Rf $KIRIN_HOME/core/ios/KirinKit/build 2>/dev/null
-node $KIRIN_HOME/build.js --ios --ios-configuration Debug
+node $KIRIN_HOME/build.js --ios --ios-configuration $configuration --no-js-build --tests none
