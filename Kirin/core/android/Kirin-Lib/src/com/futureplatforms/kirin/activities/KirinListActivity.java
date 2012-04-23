2cca1ffbec958c8911ffa422b8cf7913ec152325
jhugman <james@hugman.tv>
2012-04-23 20:57:19 +0100

    Added helper super classes.

---
+++ Kirin/core/android/Kirin-Lib/src/com/futureplatforms/kirin/activities/KirinListActivity.java
@@ -0,0 +1,50 @@
+package com.futureplatforms.kirin.activities;
+
+import com.futureplatforms.kirin.Kirin;
+import com.futureplatforms.kirin.helpers.IKirinApplication;
+import com.futureplatforms.kirin.helpers.KirinScreenHelper;
+
+import android.app.ListActivity;
+import android.content.Intent;
+
+public class KirinListActivity extends ListActivity {
+protected KirinScreenHelper mKirinHelper;
+	
+	protected void bindScreen(String moduleName) {
+		bindScreenWithoutLoading(moduleName);
+		mKirinHelper.onLoad();
+	}
+	
+	protected void bindScreenWithoutLoading(String moduleName) {
+		mKirinHelper = getKirin().bindScreen(moduleName, this);
+	}
+	
+	
+	protected Kirin getKirin() {
+		return ((IKirinApplication) this.getApplication()).getKirin();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mKirinHelper.onPause();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mKirinHelper.onResume();
+	}
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		mKirinHelper.onActivityResult(requestCode, resultCode, data);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		mKirinHelper.onUnload();
+	} 
+}
