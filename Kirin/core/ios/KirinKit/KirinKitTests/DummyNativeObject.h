9988f16bce23b9092ef835aa76ad262501799c61
jhugman <james@hugman.tv>
2012-04-24 11:38:06 +0100

    Initial commit of DummyNativeObject.

---
+++ Kirin/core/ios/KirinKit/KirinKitTests/DummyNativeObject.h
@@ -0,0 +1,13 @@
+//
+//  DummyNativeObject.h
+//  KirinKit
+//
+//  Created by James Hugman on 24/04/2012.
+//  Copyright (c) 2012 Future Platforms. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DummyNativeObject : NSObject
+
+@end
---
+++ Kirin/core/ios/KirinKit/KirinKitTests/DummyNativeObject.m
@@ -0,0 +1,13 @@
+//
+//  DummyNativeObject.m
+//  KirinKit
+//
+//  Created by James Hugman on 24/04/2012.
+//  Copyright (c) 2012 Future Platforms. All rights reserved.
+//
+
+#import "DummyNativeObject.h"
+
+@implementation DummyNativeObject
+
+@end
