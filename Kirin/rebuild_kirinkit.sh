8db23779b8940afddf5db91fa09a2455c870c370
jhugman <james@hugman.tv>
2012-04-25 13:27:14 +0100

    Use env to look up node.

--- Kirin/rebuild_kirinkit.sh
+++ Kirin/rebuild_kirinkit.sh
@@ -7,5 +7,5 @@ if [[ "$configuration" = "" ]] ; then
 fi
 rm -Rf $KIRIN_HOME/core/ios/KirinKit/build 2>/dev/null
 for conf in $configuration ; do 
-	node $KIRIN_HOME/build.js --ios --ios-configuration $conf --no-js-build --tests none
+	env node $KIRIN_HOME/build.js --ios --ios-configuration $conf --no-js-build --tests none
 done
