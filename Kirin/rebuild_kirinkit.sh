39e4f6df0ed1ee61237ce093148171ba59a8c1d6
jhugman <james@hugman.tv>
2012-04-25 13:31:07 +0100

    Configure where node is.

--- Kirin/rebuild_kirinkit.sh
+++ Kirin/rebuild_kirinkit.sh
@@ -1,11 +1,14 @@
 if [[ "$KIRIN_HOME" = "" ]] ; then 
 	export KIRIN_HOME=`dirname $0`
 fi
+if [[ "$NODE" = "" ]] ; then 
+	export NODE=`which node`
+fi
 configuration=$1
 if [[ "$configuration" = "" ]] ; then 
 	configuration="Debug Release"
 fi
 rm -Rf $KIRIN_HOME/core/ios/KirinKit/build 2>/dev/null
 for conf in $configuration ; do 
-	env node $KIRIN_HOME/build.js --ios --ios-configuration $conf --no-js-build --tests none
+	$NODE $KIRIN_HOME/build.js --ios --ios-configuration $conf --no-js-build --tests none
 done
