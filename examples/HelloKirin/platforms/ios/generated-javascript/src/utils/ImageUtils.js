82575d6f99597c7fc9be122ebec89548a7bac1a7
jhugman <james@hugman.tv>
2012-05-12 16:39:57 +0100

    Renamed demos to examples.

---
+++ examples/HelloKirin/platforms/ios/generated-javascript/src/utils/ImageUtils.js
@@ -0,0 +1,24 @@
+defineModule("ImageUtils", function (require, exports) {
+	exports.getFileTypeFromExtension = function (name) {
+		var re = /\.(png|jpg|jpeg)$/;
+		var match = re.test(name);
+		
+		var type = "png";
+		
+		if (match) {
+		
+		    switch (match[1]) {
+		
+        		case "png":
+					type = "png";
+					break;
+				case "jpeg":
+				case "jpg":
+					type = "jpeg";
+					break;
+			}
+		}	
+		return type;
+	};
+	
+});
