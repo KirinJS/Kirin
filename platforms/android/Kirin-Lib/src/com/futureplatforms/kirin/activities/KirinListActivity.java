00d6339db9598b7d5fb200db3575bb4e0cea9ff7
jhugman <james@hugman.tv>
2012-05-23 12:14:58 +0100

    Copied over from KirinActivity.
    There has to be a better way than this.

--- platforms/android/Kirin-Lib/src/com/futureplatforms/kirin/activities/KirinListActivity.java
+++ platforms/android/Kirin-Lib/src/com/futureplatforms/kirin/activities/KirinListActivity.java
@@ -8,43 +8,51 @@ import android.app.ListActivity;
 import android.content.Intent;
 
 public class KirinListActivity extends ListActivity {
-protected KirinScreenHelper mKirinHelper;
-	
+	protected KirinScreenHelper mKirinHelper;
+
+	protected <T> T bindScreen(String moduleName, Class<T> javascriptModule) {
+		bindScreen(moduleName);
+		return mKirinHelper.createProxy(javascriptModule);
+	}
+
 	protected void bindScreen(String moduleName) {
 		bindScreenWithoutLoading(moduleName);
 		mKirinHelper.onLoad();
 	}
-	
+
 	protected void bindScreenWithoutLoading(String moduleName) {
 		mKirinHelper = getKirin().bindScreen(moduleName, this);
 	}
-	
-	
+
 	protected Kirin getKirin() {
 		return ((IKirinApplication) this.getApplication()).getKirin();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
 		mKirinHelper.onPause();
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
+		if (mKirinHelper == null) {
+			throw new IllegalStateException(
+					"The activity has no javascript module bound to it. Did you call bindScreen()?");
+		}
 		mKirinHelper.onResume();
 	}
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
 		mKirinHelper.onActivityResult(requestCode, resultCode, data);
 	}
-	
+
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 		mKirinHelper.onUnload();
-	} 
+	}
 }
