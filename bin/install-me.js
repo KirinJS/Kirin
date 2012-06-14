64e480c3df5c892d688d81871d389092bdfdd905
Matt Oakes <matt@ribot.co.uk>
2012-06-14 16:27:27 +0100

    Update the android library project while installing
    While installing it now updates the android library project to add files
    which ant uses to build from the command line. These usually include the
    path which points to where the android SDK is on the machine that kirin
    was just installed on. This simplifies the process of using kirin from
    the command line by removing a step that you'd usually have to do
    manually.

--- bin/install-me.js
+++ bin/install-me.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-console.log("Now tell Eclipse and XCode that KIRIN_HOME = " + require("path").resolve(__dirname, ".."));
+var kirinHomePath = require("path").resolve(__dirname, "..");
+
+console.log("Creating ant library project files...");
+var androidUpdateProjectCommand = "android update lib-project --path " + kirinHomePath + "/platforms/android/Kirin-Lib";
+require('child_process').exec(androidUpdateProjectCommand, function(error, stdout, stderr) {
+    console.log("Now tell Eclipse and XCode that KIRIN_HOME = " + kirinHomePath);
+});
