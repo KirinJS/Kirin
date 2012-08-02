383eb744d9a57dfa001c1fbced5ab3a108e34069
Douglas Hoskins <douglas.hoskins@futureplatforms.com>
2012-08-02 08:27:27 +0100

    Changing settings on iOS to be stored with prefix 'kirin-' to avoid confusion (and potential deserialization issues) with any keys entered in to NSUserDefaults not by kirin

--- platforms/ios/KirinKit/KirinKit/Extensions/SettingsBackend.m
+++ platforms/ios/KirinKit/KirinKit/Extensions/SettingsBackend.m
@@ -1,20 +1,27 @@
 /*
-   Copyright 2011 Future Platforms
+ Copyright 2011 Future Platforms
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
-
+/**
+ *  Settings on iOS are implemented using NSUserDefaults and sent to the
+ *  rest of kirin using its JSONRepresentation.
+ *
+ *  This can cause problems if another part of the app sticks something in 
+ *  NSUserDefaults that the JSON library can't deal with.  So we only deal
+ *  with keys beginning with "kirin-".
+ */
 
 #import "SettingsBackend.h"
 #import "Kirin.h"
@@ -29,12 +36,12 @@
 @implementation SettingsBackend
 
 - (id) init {
-    return [super initWithModuleName:@"app-preferences"];
+    return [super initWithModuleName:@"Settings"];
 }
 
 - (void) onLoad {
     [super onLoad];
-
+    
     [self.kirinHelper jsMethod:@"mergeOrOverwrite" withArgsList:[[self settingsAsDictionary] JSONRepresentation]];
     [self.kirinHelper jsMethod:@"resetEnvironment"];
 }
@@ -43,7 +50,17 @@
 #pragma mark Interal Methods
 
 - (NSDictionary*) settingsAsDictionary {
-    return [[NSUserDefaults standardUserDefaults] dictionaryRepresentation];
+    // Returns a dictionary of all the pairs in NSUserDefaults
+    // whose keys begin with "kirin-"
+    NSDictionary* settings = [[NSUserDefaults standardUserDefaults] dictionaryRepresentation];
+    NSMutableDictionary* kirinSettings = [[NSMutableDictionary alloc] init];
+    
+    for (id key in settings) {
+        if ([key hasPrefix:@"kirin-"]) {
+            [kirinSettings setValue:[settings objectForKey:key] forKey:[key substringFromIndex:6]];
+        }
+    }
+    return kirinSettings;
 }
 
 #pragma mark -
@@ -57,22 +74,29 @@
 - (void)updateContents:(NSDictionary *)adds withDeletes:(NSArray *)deletes
 {
     NSUserDefaults* userSettings = [NSUserDefaults standardUserDefaults];
-
+    
+    if([adds isKindOfClass:[NSDictionary class]]) {
+        // Make a separate dictionary of all the keys with "kirin-"
+        // stuck on the front.
+        NSMutableDictionary* kirinAdds = [[NSMutableDictionary alloc] init];
+        for (id key in adds) {
+            id value = [adds objectForKey:key];
+            [kirinAdds setValue:value forKey:[NSString stringWithFormat:@"kirin-%@", key]];
+        }
+        
+        [userSettings setValuesForKeysWithDictionary:kirinAdds];
+    } else {
+        NSLog(@"[SettingsBackend] didn't expect a %@", [adds class]);
+    }
     
     if([deletes isKindOfClass:[NSArray class]]) {
         for (id key in deletes) {
-            [userSettings removeObjectForKey:key];
+            // Ensure we stick "kirin-" on the front before removing keys
+            [userSettings removeObjectForKey:[NSString stringWithFormat:@"kirin-%@", key]];
         }        
     } else {
         NSLog(@"[SettingsBackend] didn't expect a %@", [deletes class]);
     }
-    
-    if([adds isKindOfClass:[NSDictionary class]]) {
-        [userSettings setValuesForKeysWithDictionary:adds];
-    } else {
-        NSLog(@"[SettingsBackend] didn't expect a %@", [adds class]);
-    }
-
     
     [userSettings synchronize];
 }
