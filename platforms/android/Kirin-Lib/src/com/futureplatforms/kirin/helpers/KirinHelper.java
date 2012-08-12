f827f861cf9aa7843d3e000571a2bf56a126cce4
John Peberdy <john@peberdy.ca>
2012-08-12 09:43:39 -0400

    Fix passing of strings with single quotes and newlines as arguments to javascript methods. Fixes #13

--- platforms/android/Kirin-Lib/src/com/futureplatforms/kirin/helpers/KirinHelper.java
+++ platforms/android/Kirin-Lib/src/com/futureplatforms/kirin/helpers/KirinHelper.java
@@ -1,9 +1,12 @@
 package com.futureplatforms.kirin.helpers;
 
+import java.io.IOException;
+import java.io.StringWriter;
 import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import org.json.JSONArray;
@@ -47,7 +50,8 @@ public class KirinHelper implements IKirinHelper {
 			mJsContext.js(MessageFormat.format(JsCommands.EXECUTE_METHOD_JS, getModuleName(), methodName));
 		} else {
 			String argsList = prepareArgs(args);
-			mJsContext.js(MessageFormat.format(JsCommands.EXECUTE_METHOD_JS_WITH_ARGS, getModuleName(), methodName, argsList));
+			mJsContext.js(MessageFormat.format(JsCommands.EXECUTE_METHOD_JS_WITH_ARGS, getModuleName(), 
+					methodName, argsList));
 		}
 	}
 
@@ -55,7 +59,7 @@ public class KirinHelper implements IKirinHelper {
 		for (int i=0, length=args.length; i<length; i++) {
 			Object arg = args[i];
 			if (arg instanceof String) {
-				arg = "'" + arg + "'";
+				arg = "'" + escapeJavaScript((String)arg) + "'";
 			} else if (arg instanceof Integer) {
 				arg = Integer.toString((Integer) arg);
 			} else if (arg instanceof Long) {
@@ -159,4 +163,93 @@ public class KirinHelper implements IKirinHelper {
 	public <T> T javascriptProxyForModule(Class<T> interfaceClass) {
 		return mProxyGenerator.javascriptProxyForModule(interfaceClass);
 	}
+
+	/**
+	 * Based on Apache Common Lang 2.6 implementation of StringEscapeUtls.escapeJavaScript.
+	 * 
+	 * Future: Use JSONStringer?
+	 * 
+	 * @param str The string to escape
+	 * @return Escaped string
+	 * @throws IOException
+	 */
+    private static String escapeJavaScript(String str) {
+    	if (str == null) {
+            return null;
+        }
+        
+    	int size = str.length();
+        StringWriter out = new StringWriter(size * 2);
+        
+        for (int i = 0; i < size; i++) {
+            char ch = str.charAt(i);
+
+            // handle unicode
+            if (ch > 0xfff) {
+                out.write("\\u" + hex(ch));
+            } else if (ch > 0xff) {
+                out.write("\\u0" + hex(ch));
+            } else if (ch > 0x7f) {
+                out.write("\\u00" + hex(ch));
+            } else if (ch < 32) {
+                switch (ch) {
+                    case '\b' :
+                        out.write('\\');
+                        out.write('b');
+                        break;
+                    case '\n' :
+                        out.write('\\');
+                        out.write('n');
+                        break;
+                    case '\t' :
+                        out.write('\\');
+                        out.write('t');
+                        break;
+                    case '\f' :
+                        out.write('\\');
+                        out.write('f');
+                        break;
+                    case '\r' :
+                        out.write('\\');
+                        out.write('r');
+                        break;
+                    default :
+                        if (ch > 0xf) {
+                            out.write("\\u00" + hex(ch));
+                        } else {
+                            out.write("\\u000" + hex(ch));
+                        }
+                        break;
+                }
+            } else {
+                switch (ch) {
+                    case '\'' :
+                    	out.write('\\');
+                        out.write('\'');
+                        break;
+                    case '"' :
+                        out.write('\\');
+                        out.write('"');
+                        break;
+                    case '\\' :
+                        out.write('\\');
+                        out.write('\\');
+                        break;
+                    case '/' :
+                    	out.write('\\');
+                        out.write('/');
+                        break;
+                    default :
+                        out.write(ch);
+                        break;
+                }
+            }
+        }
+
+        return out.toString();
+    }
+
+    private static String hex(char ch) {
+        return Integer.toHexString(ch).toUpperCase(Locale.ENGLISH);
+    }
 }
